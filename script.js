865834b27a8edd6d2c3aff5a0befa1607b8f39a5
Mahdi Bozorg <102772767+mehtisup@users.noreply.github.com>
2023-12-01 10:15:00 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,470 @@
+
+
+document.getElementById("check").addEventListener("click", checkFunction);
+
+function checkFunction() {
+    document.getElementById("body").classList.toggle("bod");
+}
+
+
+
+
+function dopeFunction() {
+    document.getElementById("man").style.color = "red";
+}
+
+function atFunction() {
+    document.getElementById('write').scrollIntoView();
+}
+
+function uFunction() {
+    document.getElementById("myAudio").play();
+    document.getElementById('id').style.color='red'
+}
+
+
+document.getElementById("myCheck").addEventListener("click", checkFunction);
+
+function checkFunction() {
+    document.getElementById("body").classList.toggle("bod");
+}
+
+function startFunction() {
+    document.getElementById("pause").style.display = "block";
+    document.getElementById("body").style.overflow = "auto";
+}
+
+function tallFunction() {
+    document.getElementById("tall").style.display = "block";
+}
+
+function dFunction() {
+    document.getElementById("bHeart").style.opacity = "1";
+    document.getElementById("bHeart").style.display = "block";
+    document.getElementById("aHeart").style.opacity = "0";
+}
+
+function pauseFunction() {
+    document.getElementById("pause").style.display = "none";
+}
+
+function aboutFunction() {
+    document.getElementById("write").scrollIntoView();
+    document.getElementById("hidden").style.display = "none";
+    document.getElementById("list").style.display = "none";
+    document.getElementById("bLink").style.display = "block";
+    document.getElementById("aLink").style.display = "none";
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    
+
+}
+
+function menuFunction() {
+    document.getElementById("list").style.display = "block";
+    document.getElementById("aLink").style.display = "block";
+    document.getElementById("bLink").style.display = "none";
+    document.getElementById("hidden").style.display = "block";
+    document.getElementById("body").style.overflow = "auto  ";
+}
+
+function xFunction() {
+    
+    document.getElementById("ourGod").style.display = "none";
+    document.getElementById("body").style.overflow = "auto";
+}
+
+
+
+function production() {
+    
+    
+    document.getElementById("ourGod").style.display = "block";
+    document.getElementById("body").style.overflow = "hidden";
+}
+
+
+
+function layerFunction() {
+    document.getElementById("layer").style.display = "block";
+    document.getElementById("list").style.display = "none";
+    document.getElementById("body").style.overflow = "hidden";
+    document.getElementById("hidden").style.display = "none";
+
+
+    var iMahdi = setInterval(layFunction, 3000);
+
+    function layFunction() {
+        document.getElementById("layer").style.display = "none";
+        document.getElementById("list").style.display = "block";
+        document.getElementById("body").style.overflow = "auto";
+        document.getElementById("hidden").style.display = "block";
+        clearInterval(iMahdi);
+    }
+}
+
+function  tion() {
+    document.getElementById("iDope").scrollIntoView();
+}
+
+
+
+function hiddenFunction() {
+    document.getElementById("hidden").style.display = "none";
+    document.getElementById("list").style.display = "none";
+    document.getElementById("aLink").style.display = "none";
+    document.getElementById("body").style.overflow = "auto";
+
+    document.getElementById("bLink").style.display = "block";
+}
+
+
+function closeFunction() {
+    document.getElementById("body").style.overflow = "auto";
+
+    document.getElementById("hidden").style.display = "none";
+    document.getElementById("list").style.display = "none";
+    document.getElementById("bLink").style.display = "block";
+    document.getElementById("aLink").style.display = "none";
+
+}
+
+
+
+function viewFunction() {
+    document.getElementById("demo").scrollIntoView();
+}
+
+function switchFunction() {
+    document.getElementById("body").style.backgroundColor = "black";
+    document.getElementById("body").style.color = "white";
+    document.getElementById("firstStreet").style.display = "block";
+    document.getElementById("secondStreet").style.display = "none";
+    document.getElementById("down").style.display = "block";
+    document.getElementById("up").style.display = "none";
+    document.getElementById("iCircle").style.transform = "translateX(30px)";
+
+}
+
+function backFunction() {
+
+
+    document.getElementById("down").style.display = "none";
+    document.getElementById("up").style.display = "block";
+    
+    document.getElementById("iCircle").style.transform = "translateX(0px)";
+    document.getElementById("body").style.backgroundColor = "white";
+    document.getElementById("body").style.color = "black";
+    document.getElementById("secondStreet").style.display = "block";
+    document.getElementById("firstStreet").style.display = "none";
+  
+}
+
+
+
+function ahFunction() {
+    document.getElementById("aHeart").style.opacity = "0";
+    document.getElementById("bHeart").style.display = "block";
+}
+
+function bhFunction() {
+    document.getElementById("aHeart").style.opacity = "1";
+    document.getElementById("bHeart").style.display = "none";
+}
+
+function boFunction() {
+    document.getElementById("iBook").style.opacity = "0";
+    document.getElementById("mBOok").style.display = "block";
+}
+
+function okFunction() {
+    document.getElementById("mBook").style.display = "none";
+    document.getElementById("iBook").style.opacity = "1";
+}
+
+
+
+function myoooFunction() {
+    document.getElementById("myOOO").classList.toggle("obi");
+    
+}
+
+function doFunction() {
+    document.getElementById("do").style.backgroundColor = "white";
+    document.getElementById("do").style.boxShadow = "0px 8px 16px 0px rgba(0,0,0,0.03)";
+}
+
+function muFunction() {
+    document.getElementById("ps").style.display = "none";
+    document.getElementById("sp").innerHTML = ",  I currently work with HTML, CSS, JavaScript, jQuery and Bootstrap 5 to prepare my products."
+    
+
+}
+
+function website() {
+    
+    document.getElementById("tunnel").style.display = "block";
+    document.getElementById("body").style.overflow = "hidden";
+}
+
+function project() {
+    document.getElementById("body").style.overflow = "hidden";
+    document.getElementById("jump").style.display = "block";
+    document.getElementById("myJump").style.display = "block";
+
+}
+
+function jumpFunction() {
+    document.getElementById("body").style.overflow = "auto";
+    
+    document.getElementById("jump").style.display = "none";
+    document.getElementById("myJump").style.display = "none";
+}
+
+function sFunction() {
+    document.getElementById('body').style.overflow = 'auto'
+}
+
+function smoke() {
+    document.getElementById("body").style.overflow = "hidden";
+    document.getElementById("smoke").style.transform = "scale(1)";
+    document.getElementById("smoke").style.opactiy = "1";
+}
+
+function get() {
+    document.getElementById("body").style.overflow = "auto";
+    
+    document.getElementById("smoke").style.transform = "scale(0)";
+    document.getElementById("smoke").style.opactiy = "0";
+}
+
+function fanFunction() {
+    document.getElementById("tunnel").style.display = "none";
+    document.getElementById("body").style.overflow = "auto";
+    window.open('file.html')
+}
+
+function backing() {
+    document.getElementById("body").style.overflow = "auto";
+    document.getElementById("new").style.display = "none";
+
+    document.getElementById("free").style.display = "none";
+}
+
+function sefrFunction() {
+    document.getElementById("id").style.opacity = "0.8";
+}
+
+function signFunction() {
+    document.getElementById("dollarSign").style.display = "block";
+    document.getElementById("jump").style.overflow = "hidden";
+}
+
+function dollarFunction() {
+    document.getElementById("dollarSign").style.display = "none";
+    document.getElementById("jump").style.overflow = "auto";
+
+}
+
+
+function udFunction() {
+    document.getElementById("ud").style.opacity = "0.8";;
+}
+
+function flexFunction() {
+    document.getElementById("flexx").style.display = "none";
+}
+
+function leFunction() {
+    document.getElementById("flexx").style.display = "block";
+    var M = setInterval(mM, 3000);
+
+    function mM() {
+        document.getElementById("flexx").style.display = "none";
+        clearInterval(M)
+    }
+
+
+    
+}
+
+
+
+function tFunction() {
+    document.getElementById("body").style.overflow = "hidden";
+    document.getElementById("free").style.opacity = "1";
+    
+    document.getElementById("free").style.display = "block";
+}
+
+function ocean() {
+    document.getElementById("body").style.overflow = "hidden";
+    document.getElementById("new").style.display = "block"
+}
+
+function kFunction() {
+    document.getElementById("k").style.backgroundColor = "springgreen";
+    document.getElementById("k").style.color = "black"
+}
+
+
+
+function imgFunction() {
+    document.getElementById("fixed").style.display = "none";
+    document.getElementById("body").style.overflow = "hidden";
+    document.getElementById("ourGod").style.display = "none"
+    document.getElementById("body").style.overflow = "auto"
+
+}
+
+
+const soundEffect = new Audio();
+soundEffect.autoplay = true;
+
+// onClick of first interaction on page before I need the sounds
+// (This is a tiny MP3 file that is silent and extremely short - retrieved from https://bigsoundbank.com and then modified)
+soundEffect.src = "https://www.w3schools.com/html/horse.ogg";
+
+// later on when you actually want to play a sound at any point without user interaction
+soundEffect.src = 'https://www.w3schools.com/html/horse.ogg';
+
+function getAudio() {
+    var audio = document.getElementById("audio");
+
+    audio.play()
+}
+
+
+function product() {
+    document.getElementById("body").style.overflow = "hidden";
+    document.getElementById("pr").style.display = "block";
+    document.getElementById("pr").style.transform = "translateY(50px)";
+    
+    document.getElementById("top").style.display = "block";
+    document.getElementById("right").style.display = "block";
+
+    document.getElementById("myPr").style.display = "block";
+
+
+}
+
+
+function backFunction() {
+    document.getElementById("body").style.overflow = "auto";
+    document.getElementById("right").style.display = "none";
+    document.getElementById("pr").style.transform = "translateY(0)";
+    document.getElementById("pr").style.display = "none";
+    document.getElementById("myPr").style.display = "none";
+    document.getElementById("top").style.display = "none";
+
+}
+
+
+
+function fixedFunction() {
+    document.getElementById("fixed").style.display = "none";
+    document.getElementById("body").style.overflow = "hidden";
+    document.getElementById("ourGod").style.display = "none";
+}
+
+
+function inFunction() {
+    document.getElementById("countdown").scrollIntoView()
+}
+
+document.getElementById("super1").style.backgroundColor = "white";
+document.getElementById("super1").style.zIndex = "100";
+
+function suFunction() {
+    document.getElementById("super1").style.backgroundColor = "white";
+
+    document.getElementById("super1").style.zIndex = "100";
+    document.getElementById("super2").style.zIndex = "99";
+
+    document.getElementById("super2").style.backgroundColor = "whitesmoke";
+}
+
+function perFunction() {
+    
+    document.getElementById("super1").style.backgroundColor = "whitesmoke";
+    document.getElementById("super2").style.backgroundColor = "white";
+    document.getElementById("super1").style.zIndex = "99";
+
+    document.getElementById("super2").style.zIndex = "100";
+
+}
+
+
+function pocket() {
+    document.getElementsByClassName("pocket").style.opacity = "1";
+    document.getElementById("pocket").style.display = "none";
+}
+
+
+
+function stopFunction() {
+    var star = setInterval(starFunction, 3000);
+
+    function starFunction() {
+        document.getElementById("dealer").style.display = "none";
+        clearInterval(star)
+    }
+
+
+}
+
+
+
+function heheFunction() {
+    var using = setInterval(usingFunction, 1000);
+
+    function usingFunction() {
+        document.getElementById("kappaa").style.display = "none"
+    }
+}
+
+
+document.getElementById("kappa").style.display = "block";
+
+
+
+function ffFunction() {
+    document.getElementById("heheh").style.display = "block";
+}
+
+function fameFunction() {
+    document.getElementById("heheh").style.display = "none";
+}
+
+
+
+
+function favorite() {
+    document.getElementById("favorite").style.display = "block";
+    document.getElementById("body").style.overflow = "hidden"
+}
+
+
+function focusFunction() {
+    document.getElementById("body").style.overflow = "auto";
+    document.getElementById("favorite").style.display = "None";
+}
+
+
+
+function ballFunction() {
+    document.getElementById("ball").style.display = "none";
+    
+    
+}
+
+
+
+
+
+function soMany() {
+    document.getElementById("rockstar").scrollIntoView();
+}
